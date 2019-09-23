b6fd94879e8a6e01997af9fb8cd836db859e2044
xim xim <tlawogus11106@gmail.com>
2019-09-23 17:27:03 +0900

    9월 23일 자바 공부

--- Chapter10/src/note/init_test/InitTest.java
+++ Chapter10/src/note/init_test/InitTest.java
@@ -2,52 +2,89 @@ package note.init_test;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class InitTest extends Application{
-	
+public class InitTest extends Application {
+
 	public InitTest() {
+		
 		System.out.println(Thread.currentThread().getName() + " Thread");
 	}
 
 	public static void main(String[] args) {
+		
 		System.out.println(Thread.currentThread().getName() + " Thread");
+		
 		launch(args);
-
 	}
-	
+
 	@Override
 	public void init() throws Exception {
+		
 		System.out.println(Thread.currentThread().getName() + " Thread");
+		
 		Parameters ps = getParameters();
 		List<String> list = ps.getRaw();
-		for(String value : list) {
+		
+		for (String value : list) {
+			
 			System.out.println(value);
 		}
-		
+
 		Map<String, String> map = ps.getNamed();
 		Set<Entry<String, String>> set = map.entrySet();
-		
-		for(Entry<String, String> entry : set) {
+
+		for (Entry<String, String> entry : set) {
+			
 			String key = entry.getKey();
 			String value = entry.getValue();
-			
+
 			System.out.println("key : " + key + " value : " + value);
 		}
 	}
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		System.out.println(Thread.currentThread().getName() + "Thread");
-		primaryStage.setTitle("xim");
-		primaryStage.show();
 		
+		System.out.println(Thread.currentThread().getName() + " Thread");
+		
+		VBox rootVBox = new VBox();
+		rootVBox.setPrefSize(400, 600);
+		rootVBox.setSpacing(20);
+		rootVBox.setAlignment(Pos.CENTER);
+
+		TextField textField = new TextField();
+		textField.setPrefWidth(200);
+
+		Button button = new Button();
+		button.setText("button");
+		button.setOnAction((e) -> Platform.exit());
+
+		ObservableList<Node> list = rootVBox.getChildren();
+		list.add(textField);
+		list.add(button);
+
+		Scene scene = new Scene(rootVBox);
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
 	}
+
 	@Override
 	public void stop() throws Exception {
+		
 		System.out.println(Thread.currentThread().getName() + " Thread");
 	}
 }
